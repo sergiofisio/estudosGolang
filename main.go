f83a9346f1734b324579f857dbfaba39803c560a
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-07-18 16:26:51 -0300

    teste deploy

--- main.go
+++ main.go
@@ -1,92 +1,22 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
-	"github.com/sergiofisio/estudosGolang/controller"
-	"github.com/sergiofisio/estudosGolang/database"
-	"github.com/sergiofisio/estudosGolang/middleware"
+    "github.com/gin-gonic/gin"
 
 )
 
-var db *sql.DB
-
-func logRequestMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        url := c.Request.URL.Scheme + "://" + c.Request.Host + c.Request.RequestURI
-        fmt.Printf("url: %s\n", url)
-        fmt.Printf("metodo: %s\n", c.Request.Method)
-        c.Next()
-    }
-}
-
 func main() {
-    if err := godotenv.Load(); err != nil {
-        log.Fatal("Erro carregando .env")
-    }
+    router := gin.Default()
 
-    connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
-    os.Getenv("DB_USER"),
-    os.Getenv("DB_PASSWORD"),
-    os.Getenv("DB_HOST"),
-    os.Getenv("DB_PORT"),
-    os.Getenv("DB_NAME"))
-
-    var err error
-    db, err = sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
-
-    err = db.Ping()
-    if err != nil {
-        log.Fatal("Erro ao conectar ao banco de dados:", err)
-    }
-
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "4000"
-    }
-
-    database.CreateTables(db)
-
-    r := gin.Default()
-    r.Use(logRequestMiddleware())
-
-    r.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "Servidor Iniciado")
+    router.POST("/register", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{"message": "Registro bem-sucedido!"})
     })
 
-    r.POST("/register", func(c *gin.Context) {
-        controller.RegisterHandler(c.Writer, c.Request, db)
+    router.POST("/login", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{"message": "Login bem-sucedido!"})
     })
 
-    r.POST("/login", func(c *gin.Context) {
-        controller.LoginHandler(c.Writer, c.Request, db)
-    })
-
-    authGroup := r.Group("/")
-    authGroup.Use(middleware.Authenticate())
-    {
-        authGroup.PUT("/update/:id", func(c *gin.Context) {
-            controller.UpdateHandler(c.Writer, c.Request, db)
-        })
-
-        authGroup.DELETE("/delete/:id", func(c *gin.Context) {
-            controller.DeleteHandler(c.Writer, c.Request, db)
-        })
-    }
-
-    fmt.Printf("Servidor iniciado na porta %s...\n", port)
-    if err := r.Run(":" + port); err != nil {
-        log.Fatal(err)
-    }
+    router.Run(":8080")
 }
