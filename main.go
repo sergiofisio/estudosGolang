ea48708945380fa982763c642242b1d74786f2fa
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-07-18 17:02:42 -0300

    init

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
     router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message":"Servidor iniciado"})
+	})
 
     router.POST("/register", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{"message": "Registro bem-sucedido!"})
